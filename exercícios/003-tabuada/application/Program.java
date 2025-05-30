ebc1998d029d3d7b8238630ea2d02e885b5ffbd2
witaloxz <witalodias1@gmail.com>
2025-05-30 10:28:41 -0300

    att: errors

--- exercícios/003-tabuada/application/Program.java
+++ exercícios/003-tabuada/application/Program.java
@@ -4,12 +4,12 @@ public class Program {
 public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
 
-    System.out.println("Digite um número: ");
+    System.out.print("Digite um número: ");
     int n1 = sc.nextInt();
 
-
-    for(int i = 0; i < n1; i++){
-        System.out.println("Tabuada de multiplicação do " + n1);
+    System.out.println("Tabuada de multiplicação do " + n1);
+    for(int i = 0; i <= 10; i++){
+        
         System.out.println(n1 + " x " + i + " = " + (n1 * i));
     }
     
